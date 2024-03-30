b2b820a1ddfd065215703a66986152c611bccff3
b-dulaney <brendanfdulaney@gmail.com>
2024-03-30 13:57:38 -0600

    chore: create separate prod/dev cron jobs and extract to util function

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
-	"powderhoundgo/internal/queue"
 	"powderhoundgo/internal/supabase"
 	"powderhoundgo/internal/util"
 	"syscall"
-	"time"
 
 	"github.com/hibiken/asynq"
-	"github.com/robfig/cron/v3"
 )
 
 func main() {
@@ -26,42 +22,12 @@ func main() {
 	client := asynq.NewClient(asynq.RedisClientOpt{Addr: redisHost + ":6379", Password: "", DB: 0})
 	defer client.Close()
 
-	loc, err := time.LoadLocation("America/Denver")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	cron := cron.New(cron.WithLocation(loc))
-	// Regular hourly web scraping jobs
-	cron.AddFunc("@hourly", func() {
-		queue.QueueResortWebScrapeTasks(client)
-	})
-
-	// Early morning web scraping jobs - checking for overnight snowfall - 5:00am - 6:00am
-	cron.AddFunc("*/10 5-6 * * *", func() {
-		queue.QueueResortWebScrapeTasks(client)
-	})
-
-	// Daily forecast alert emails - 4:30pm
-	cron.AddFunc("30 16 * * *", func() {
-		queue.QueueForecastAlertEmailTasks(client, supabase)
-	})
-
-	// Overnight alert emails - 6:05am
-	cron.AddFunc("5 6 * * *", func() {
-		queue.QueueOvernightAlertEmailTasks(client, supabase)
-	})
+	cron := util.InitializeCronTasks(client, supabase)
 
 	go cron.Start()
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
 	<-sig
 
-	printCronEntries(cron.Entries())
-
 	select {}
-}
-
-func printCronEntries(cronEntries []cron.Entry) {
-	log.Printf("Cron Info: %+v\n", cronEntries)
 }
--- internal/util/utils.go
+++ internal/util/utils.go
@@ -2,8 +2,14 @@ package util
 
 import (
 	"log"
+	"os"
+	"powderhoundgo/internal/queue"
+	"powderhoundgo/internal/supabase"
+	"time"
 
+	"github.com/hibiken/asynq"
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 )
 
 func LoadEnvironmentVariables() {
@@ -11,4 +17,64 @@ func LoadEnvironmentVariables() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
+}
+
+func InitializeCronTasks(client *asynq.Client, supabase supabase.SupabaseClient) *cron.Cron {
+	ENV := os.Getenv("ENV")
+	loc, err := time.LoadLocation("America/Denver")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cron := cron.New(cron.WithLocation(loc))
+
+	if ENV == "production" {
+		addProductionCronTasks(cron, client, supabase)
+	} else {
+		addDevelopmentCronTasks(cron, client, supabase)
+	}
+
+	printCronEntries(cron.Entries())
+
+	return cron
+}
+
+func addProductionCronTasks(cron *cron.Cron, client *asynq.Client, supabase supabase.SupabaseClient) {
+	// Regular hourly web scraping jobs
+	cron.AddFunc("@hourly", func() {
+		queue.QueueResortWebScrapeTasks(client)
+	})
+
+	// Early morning web scraping jobs - checking for overnight snowfall - 5:00am - 6:00am
+	cron.AddFunc("*/10 5-6 * * *", func() {
+		queue.QueueResortWebScrapeTasks(client)
+	})
+
+	// Daily forecast alert emails - 4:30pm
+	cron.AddFunc("30 16 * * *", func() {
+		queue.QueueForecastAlertEmailTasks(client, supabase)
+	})
+
+	// Overnight alert emails - 6:05am
+	cron.AddFunc("5 6 * * *", func() {
+		queue.QueueOvernightAlertEmailTasks(client, supabase)
+	})
+}
+
+func addDevelopmentCronTasks(cron *cron.Cron, client *asynq.Client, supabase supabase.SupabaseClient) {
+	cron.AddFunc("@every 1m", func() {
+		queue.QueueResortWebScrapeTasks(client)
+	})
+
+	cron.AddFunc("@every 1m", func() {
+		queue.QueueForecastAlertEmailTasks(client, supabase)
+	})
+
+	cron.AddFunc("@every 1m", func() {
+		queue.QueueOvernightAlertEmailTasks(client, supabase)
+	})
+}
+
+func printCronEntries(cronEntries []cron.Entry) {
+	log.Printf("Cron Info: %+v\n", cronEntries)
 }
